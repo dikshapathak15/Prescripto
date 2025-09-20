dbbc4a56c81c873fc68ef1e2d21c934860223390
dikshapathak15 <dikshapathak1541@gmail.com>
2025-09-21 01:50:10 +0530

    added footer

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Doctors = () => {
+  const {speciality} = useParams();
+  console.log
   return (
-    <div>Doctors</div>
+    <div></div>
   )
 }
 
